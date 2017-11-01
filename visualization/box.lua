4320aa610b7078af234138233bab087c4aec13ad
grunkgrunk <danielgrunkin@gmail.com>
2017-11-01 19:33:51 +0100

    removed unnecessary code

--- visualization/box.lua
+++ visualization/box.lua
@@ -1,30 +1,27 @@
 local class = require 'util.middleclass'
-local Timer=require 'util.timer'
+local Timer = require 'util.timer'
 
 local flr = math.floor
 local round = function(n) return flr(n+0.5) end
 
 local Box = class('Box')
 
-Box.static.colors = {
+local colors = {
   min = 0,
   normal = 16,
-  select = 2,
-  equal = 7,
-  highlight = 0
 }
 
-function Box:initialize(number, s)
+function Box:initialize(number, s, shack)
+  self.number = number
+  self.s = s or 16
+  self.shack = shack
+
   self.x = 0
   self.y = 0
   self.issmallest = false
-
-  self.s = s or 16
-  self.number = number
-
   self.timer = Timer.new()
+  self.color = colors.normal
 
-  self.color = Box.colors.normal
 end
 
 function Box:drawConnection()
@@ -39,10 +36,13 @@ function Box:draw()
 
   color(self.color)
   local strnum = tostring(self.number)
-  love.graphics.print(self.number, round(self.x+self.s/2-2*#strnum)
-  ,round(self.y+self.s/2-2))
+  love.graphics.print(
+    self.number,
+    round(self.x+self.s/2-2*#strnum),
+    round(self.y+self.s/2-2)
+  )
 
-  if self.issmallest then self:drawOutline(Box.colors.min) end
+  if self.issmallest then self:drawOutline(colors.min) end
 end
 
 function Box:drawOutline(c)
@@ -56,7 +56,7 @@ function Box:update(dt)
 end
 
 function Box:move(x,y, t, fn)
-  self.timer:tween(t or 0.2 + math.random(100)/200, self, {x=x or self.x, y=y or self.y}, 'in-out-quad', fn)
+  self.timer:tween(t or 0.4, self, {x=x or self.x, y=y or self.y}, 'in-out-quad', fn)
 end
 
 function Box:tweenFn(tbl, t)
@@ -65,41 +65,21 @@ function Box:tweenFn(tbl, t)
   end
 end
 
-function Box:select(s,compare)
-  self.color = s and Box.colors.select or Box.colors.normal
-end
-
 function Box:compare(with)
   local r = self.n > with.n
   return r
 end
 
 function Box:swap(x, sgn, wait)
-  --local up = self:tweenFn({y=32*sgn}, 0.2)
   local to = self:tweenFn({x=x}, 0.2)
   local down = self:tweenFn({y=0}, 0.2)
-  --shack:shake(5)
-  --up()
---  wait(0.5)
+
   to()
   wait(0.5)
   down()
   wait(0.1)
-  shack:shake(10)
+  self.shack:shake(10)
   wait(0.2)
-  -- --up()
-  -- self.timer:tween(0.2, self, {y=24 * sgn or -1}, 'linear'
-  -- ,function()
-  --   print(self.x)
-  --   self.timer:tween(t or 0.3, self, {x=b.x}, 'linear',
-  --   function() self.timer:tween(0.2, self, {y=0}, 'linear'); print(self.x) end)
-  -- end)
-  -- --)
-end
-
--- should not do this
-function Box:calcX(i)
-  return i*(o.s+1)
 end
 
 return Box
--- visualization/main.lua
+++ visualization/main.lua
@@ -1,32 +1,23 @@
 
-shack=require'util.shack'
-lume=require'util.lume'
-Timer=require'util.timer'
-tween=require'util.tween'
-Vector=require'util.vector'
+local shack=require'util.shack'
+local lume=require'util.lume'
+local Timer=require'util.timer'
 
 local Box = require 'box'
 local Cursor = require 'cursor'
 
-love.graphics.setLineStyle "rough"
-love.graphics.setDefaultFilter("nearest", "nearest")
+local rnd, abs, flr = love.math.random, math.abs, math.floor
 
-scale = 6
+love.graphics.setLineStyle("rough")
+local canvas = love.graphics.newCanvas(128, 128)
+canvas:setFilter("nearest","nearest")
 
+local sin,cos,tan = math.sin,math.cos,math.tan
+
+local scale = 6
 love.window.setMode(128 *scale, 128 * scale, {borderless=true})
 
-local rnd, abs, flr = love.math.random, math.abs, math.floor
-local mx, my = love.mouse.getX, love.mouse.getY
-
-
-local screen = love.graphics.newCanvas(128, 128)
-local objectScreen = love.graphics.newCanvas(128,128)
-
-local strength = 1000 -- The more circles the stronger the effect.
-local sin,cos,tan = math.sin,math.cos,math.tan
-t=0
-
-palette = {
+local palette = {
   {0,0,0},
   {29,43,83},
   {126,37,83},
@@ -45,48 +36,59 @@ palette = {
   {255,204,170}
 }
 
--- palettes
-bg = {15, 10, 9, 4, 2}
+function BEGIN_DRAW()
+  local function rgbtonum(r,g,b)
+    for i,col in ipairs(palette) do
+      local r1,g1,b1=col[1],col[2],col[3]
+      if r==r1 and g==g1 and b==b1 then return i-1 end
+    end
+  end
 
-function map(x, a,b, c,d)
-  return (x-a)/(b-a)*(d-c)+c
+  local function findi(t,val)
+    for i,v in ipairs(t) do
+      if v == val then return i end
+    end
+    return t[#t]
+  end
+
+  love.graphics.setCanvas(canvas)
+  local data = canvas:newImageData()
+  for i = 1, 1000 do
+    local x,y=rnd(0,127),rnd(0,127)
+    local clearcol=15
+    -- If we want to do something based on the color at the pixel x,y
+    --if math.random(1000) > 600 then
+    --  local r,g,b=data:getPixel(x,y)
+    --  local c=rgbtonum(r,g,b)
+    --  local newcol = bg[findi(bg, c) + math.random(-1, 1)]
+      --if newcol then clearcol = newcol end
+    --end
+
+    -- the dithering effect
+    color(clearcol)
+    love.graphics.points(x+1,y, x-1,y, x,y+1,  x,y-1)
+  end
+end
+
+function END_DRAW()
+  love.graphics.setCanvas()
+  love.graphics.setColor (255,255,255)
+  love.graphics.setBlendMode("alpha", "premultiplied")
+  shack:apply()
+  love.graphics.draw(canvas,0,0,0,scale,scale)
 end
 
 function color(i)
-  -- should use modulo better
   love.graphics.setColor(palette[flr(i) % 16 + 1])
 end
-
-function pal(c1, c2)
-  local a,b=palette[c1],palette[c2]
-  --palette[]
-end
-
-function dither(x,y)
-  love.graphics.points(x+1,y, x-1,y, x,y+1,  x,y-1)
-end
-
-function findi(t,val)
-  for i,v in ipairs(t) do
-    if v == val then return i end
-  end
-  return t[#t]
-end
-
-function rgbtonum(r,g,b)
-  for i,col in ipairs(palette) do
-    local r1,g1,b1=col[1],col[2],col[3]
-    if r==r1 and g==g1 and b==b1 then return i-1 end
-  end
-end
-
 
 function swap(arr, i,j)
   local a,b = arr[i], arr[j]
   arr[i],arr[j] = b,a
 end
 
-function stopSort()
+function stopsort()
+  sorting = false
   Timer.clear()
   Timer.script(function(wait)
     wait(0.2)
@@ -95,8 +97,6 @@ function stopSort()
       local x = (i-1) * (b.s)
       b.timer:clear()
       b:move(x)
-
-
     end
     wait(0.5)
     revert()
@@ -104,7 +104,84 @@ function stopSort()
   end)
 end
 
-between=0.5
+function revert(selected, minbox)
+  lume.each(boxes, function(b)
+    if b ~= selected and b ~= minbox then
+      b.issmallest = false
+      b:move(b.x, 0, 0.5)
+    end
+  end)
+end
+
+function sortboxes()
+  sorting = true
+  sortHandle = Timer.script(function(wait)
+    local curri=1
+    local selected = boxes[curri]
+    wait(0.2)
+
+    while true do
+      if curri > #boxes-1 then
+        sorting = false
+        shack:shake(5)
+        return false
+      end
+
+      selected = boxes[curri]
+      cursor.box = selected
+      cursor.i = curri-1
+
+      wait(0.2)
+
+      local i = curri
+      local currnum= selected.number
+
+      local min = {i=curri, box=selected}
+
+      while true do
+        i = i + 1
+        local box = boxes[i]
+
+        if i > #boxes then break end
+
+        if box.number < min.box.number then
+          if min.box ~= selected then
+            min.box.issmallest = false
+          end
+          min.i, min.box = i, box
+          min.box.issmallest = true
+        end
+
+        local to = (currnum - box.number)*box.s/2
+        local time = 0
+        if to ~= 0 then
+          time = abs(to)/80
+          box:move(nil, to, time)
+        end
+        wait(time + 0.1)
+      end
+
+      -- allign everything again
+      revert(selected, min.box)
+
+      if min.box ~= selected then
+        wait(0.2)
+        -- swapping time!
+        swap(boxes, curri, min.i)
+
+        local x1, x2 = min.box.x, selected.x
+        selected:move(nil, -16+2)
+        min.box:move(nil, 16+2)
+        wait(0.3)
+        selected:swap(x1, -1, wait)
+        min.box:swap(x2, 1, wait)
+      end
+      min.box.issmallest = false
+      curri=curri + 1
+      selected=boxes[curri]
+    end
+  end)
+end
 
 function love.load()
   local font=love.graphics.newFont('font.ttf', 4)
@@ -112,122 +189,19 @@ function love.load()
   boxes={}
 
   for i = 0,10 do
-    local b = Box:new(math.random(10), 10)
+    local b = Box:new(math.random(1,14), 10, shack)
     b.x = i * (b.s)
     b.y = 64
-    b:move(nil, 0)
+    b:move(nil, 0, math.random()*0.4+0.1)
 
     boxes[#boxes+1]=b
   end
 
-  cursor = Cursor:new(boxes)
-
-  function nextBox(i)
-    return i+1, boxes[i+1]
-  end
-
-  function compare(a,b)
-    if a==b then return 0 end
-    return a < b
-  end
-
-  function revert(selected, minbox)
-    lume.each(boxes, function(b)
-      if b ~= selected and b ~= minbox then
-        b.color = Box.colors.normal
-        b.issmallest = false
-        b:move(b.x, 0, 0.5)
-      end
-    end)
-  end
-
-
+  cursor = Cursor:new(boxes, shack)
   sorting = false
-  function sortBoxes()
-    sorting = true
-    sortHandle = Timer.script(function(wait)
-      --curri, selected=nextBox(curri)
-      local curri=1
-      local selected = boxes[curri]
-      --selected.color = Box.colors.select
-      wait(0.2)
-
-      while true do
-        if curri > #boxes-1 then
-          sorting = false
-          shack:shake(5)
-          return false
-        end
-
-
-        selected = boxes[curri]
-        cursor.box = selected
-        cursor.i = curri-1
-
-        wait(0.2)
-
-        local i = curri
-        local currnum= selected.number
-
-        local min = {i=curri, box=selected}
-
-        while true do
-          i = i + 1
-          local box = boxes[i]
-
-          if i > #boxes then break end
-          --local c=compare(currn,n)
-
-          if box.number == selected.number then
-            --box.color = Box.colors.equal
-
-          end
-
-          if box.number < min.box.number then
-            if min.box ~= selected then
-              min.box.color = Box.colors.normal
-              min.box.issmallest = false
-            end
-            min.i, min.box = i, box
-            --min.box.color = Box.colors.min
-            min.box.issmallest = true
-          end
-
-          box:move(nil, (currnum - box.number)*box.s/2)
-          wait(0.6)
-        end
-
-        -- allign everything again
-        revert(selected, min.box)
-
-        if min.box ~= selected then
-          wait(0.2)
-          -- swapping time!
-          swap(boxes, curri, min.i)
-          --Box.swap(selected, min.box, 1)
-          local x1, x2 = min.box.x, selected.x
-          selected:move(nil, -16+2)
-          min.box:move(nil, 16+2)
-          wait(0.3)
-          selected:swap(x1, -1, wait)
-          min.box:swap(x2, 1, wait)
-          --wait(0.5)
-        end
-        min.box.issmallest = false
-        --selected.color = Box.colors.normal
-        --wait(0.5)
-        curri=curri + 1
-        selected=boxes[curri]
-
-        --if not sorting then return false end
-
-      end
-    end)
-  end
 end
 
 function love.update(dt)
-  t = t + dt
   shack:update(dt)
   Timer.update(dt)
 
@@ -238,8 +212,6 @@ function love.update(dt)
 end
 
 function love.draw()
---love.graphics.push()
-
 BEGIN_DRAW()
 
   love.graphics.push()
@@ -247,15 +219,13 @@ BEGIN_DRAW()
   local l=#boxes*s
   love.graphics.translate(63-l/2, 64)
 
-  -- draw the line behind
+  -- draw the lines behind the boxes
   color(0)
   love.graphics.line(s/2, s/2, l-s/2, s/2)
   lume.each(boxes, function(b) b:drawConnection() end)
-  local txt ="PRESS X TO STOP."
-  if not sorting then
-    txt="PRESS X TO SORT!"
-  end
+
   color(0)
+  local txt = sorting and "PRESS X TO STOP." or "PRESS X TO SORT!"
   love.graphics.print(txt, #txt*3/2, -32)
 
   lume.each(boxes, function(b) b:draw() end)
@@ -266,48 +236,7 @@ BEGIN_DRAW()
 END_DRAW()
 end
 
-function BEGIN_DRAW()
-
-  love.graphics.setCanvas(screen)
-
-  local data=screen:newImageData()
-  for i = 1, 1000 do
-    local x,y=flr(rnd(0,127)),flr(rnd(0,127))
-    -- this is stupid
-
-    local col=15
-    --if math.random(1000) > 600 then
-    --  local r,g,b=data:getPixel(x,y)
-    --  local c=rgbtonum(r,g,b)
-    --  local newcol = bg[findi(bg, c) + math.random(-1, 1)]
-      --if newcol then col = newcol end
-    --end
-    color(col)
-    dither(x, y+1)
-    love.graphics.points(x,y)
-
-  end
-
-  love.graphics.setCanvas(screen)
-  love.graphics.setColor(255,255,255)
-  --love.graphics.clear()
-
-end
-
-function END_DRAW() -- render frame.
-  --love.graphics.setCanvas(screen)
-  --love.graphics.setColor (255,255,255)
-
-  love.graphics.setCanvas()
-  love.graphics.clear()
-  love.graphics.setColor (255,255,255)
-
-  shack:apply()
-  love.graphics.draw(screen,0,0,0,scale,scale)
-  love.graphics.draw(objectScreen,0,0,0,scale,scale)
-end
-
-function love.keypressed(key, code, isrepeat)
+function love.keypressed(key)
   if key == "escape" then love.event.quit() end
 
   if not sorting then
@@ -326,14 +255,9 @@ function love.keypressed(key, code, isrepeat)
   end
   if key == "x" then
     if sorting then
-      --Timer.clear()
-      --revert()
-      stopSort()
-      sorting = false
-
+      stopsort()
     else
-      sortBoxes()
+      sortboxes()
     end
   end
-
 end
